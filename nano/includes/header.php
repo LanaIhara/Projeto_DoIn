71092d6049f1d2a1e7cde8e8587f249e36620210
lana <lanatriforce1234@gmail.com>
2022-11-17 15:11:22 -0300

    .

--- nano/includes/header.php
+++ nano/includes/header.php
@@ -1,20 +1,20 @@
 <!-- Cabeçalho -->
-<header class="cabeçalho">
+<header class="header">
 
     <!-- Logotipo do site DoIn-->
-    <a href="#" class="img_slogan"><img class="logotype" src="img/doin_logotype.png"></a>
+    <a href="#" class="div_logo"><img class="img_logo" src="img/doin_logotype.png"></a>
 
     <!-- Seção de pesquisa -->
     <div class="dropdown">
-        <div class="indice_items">
+        <div class="button_hover">
             <!-- Título -->
                 Páginas do site
                 
             <!-- Símbolo da lupa de pesquisa -->
-            <img class="symbol" src="img/search.png">
+            <img class="img_symbol" src="img/search.png">
         </div>
             
-        <div class="dropdown-content" id="pag_content_drop">
+        <div class="dropdown-content" id="pag_content">
             <a href="#">Doações e Trocas</a>
             <a href="#">Ranking dos doadores</a> <!-- ou Top usuários do site -->
             <a href="#">Página inicial</a>
@@ -23,13 +23,13 @@
     <!-- Fonte: https://blog.hubspot.com/website/html-dropdown#:~:text=A%20drop%2Ddown%20menu%20is,user%20disengages%20from%20the%20menu. -->
 
     <!-- Sumário -->
-    <div class="dropdown" id="menu_nav">
-        <div class="indice_items">
+    <div class="dropdown" id="menu">
+        <div class="button_hover">
             <!-- Título -->
             Menu de navegação
                 
             <!-- Símbolo da lupa de pesquisa -->
-            <img class="symbol" src="img/menu.png">
+            <img class="img_symbol" src="img/menu.png">
         </div>
         <div class="dropdown-content">
             <a href="#sonic">Categorias</a>
@@ -39,14 +39,14 @@
         </div>
     </div>
 
-    <div class="teste">
-        <div class="info">
-            <a href="#" class="a_button"><div class="detalhe button"> <!-- indice_items adicional -->    
+    <div class="div_right">
+        <div class="home_contact">
+            <a href="#" class="a_button"><div class="purple_button button"> <!-- indice_items adicional -->    
                 <!-- Símbolo da lupa de pesquisa -->
-                <img class="symbol home" src="img/home.png">
+                <img class="img_symbol img_home" src="img/home.png">
             </div></a>
 
-            <a href="#" class="a_button"><div class="detalhe button adicional">
+            <a href="#" class="a_button"><div class="purple_button button espac_contact">
                 <!-- Título -->
                 Sobre nós
             </div></a>
