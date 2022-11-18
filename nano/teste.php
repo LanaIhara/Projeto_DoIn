6f3570227974cde1fcdee04981deac6499ba9d1d
lana <lanatriforce1234@gmail.com>
2022-11-18 09:39:29 -0300

    .

---
+++ nano/teste.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" /> <!-- Torna o site na linguagem pt-BR (português - Brasil), sendo possível utilizar acentos -->
+	<title>DoIn</title> <!-- Título do site -->
+
+    <link rel="stylesheet" type="text/css" href="css/zero_page.css"/>
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/teste_css.css" />
+
+    <script type="text/javascript" src="js/jquery-3.6.1.min.js"></script> <!-- refere-se ao jQuery -->
+    <link rel="icon" type="image/png" href="img/favicon.png" /> <!-- refere-se ao favicon.png -->
+
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    /> <!-- animate.style -->
+</head>
+
+    <!-- Estrutura do site/Conteúdo do site -->
+    <body>
+        <!-- Início da página do usuário -->
+        <div class="primeiro">
+            <div class="sinonimo">
+                <div class="antonimo"></div>
+                <div class="teste">
+                    <div class="info"></div>
+                    <div class="info"></div>
+                    <div class="info"></div>
+                    <div class="info"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
