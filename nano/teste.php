8146f7b02ccbfd006a5a5a86fabf63d8750eec29
lana <lanatriforce1234@gmail.com>
2022-11-25 09:42:57 -0300

    .

--- nano/teste.php
+++ nano/teste.php
@@ -38,7 +38,85 @@
                     <img src="img/pink.jpg" class="img_category">
                     <label>Beleza</label>
                 </div>
-                
+
+                <!-- 2: Saúde -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Saúde</label>
+                </div>
+
+                <!-- 3: Casa -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Casa</label>
+                </div>
+
+                <!-- 4: Eletrodomésticos -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Eletrodomésticos</label>
+                </div>
+
+                <!-- 5: Móveis -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Móveis</label>
+                </div>
+
+                <!-- 6: Decoração -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Decoração</label>
+                </div>
+
+                <!-- 7: Alimentos -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Alimentos</label>
+                </div>
+
+                <!-- 8: Bebidas -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Bebidas</label>
+                </div>
+
+                <!-- 9: Esporte -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Esporte</label>
+                </div>
+
+                <!-- 1: Beleza -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Beleza</label>
+                </div>
+
+                <!-- 1: Beleza -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Beleza</label>
+                </div>
+
+                <!-- 1: Beleza -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Beleza</label>
+                </div>
+
+                <!-- 1: Beleza -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Beleza</label>
+                </div>
+
+                <!-- 1: Beleza -->
+                <div class="div_category">
+                    <img src="img/pink.jpg" class="img_category">
+                    <label>Beleza</label>
+                </div>
+
             </div>
         </section>
     
