bf0ce927b545d1bfe38a54c8230d5d1b72a81ea5
nano <lanatriforce1234@gmail.com>
2022-12-05 22:03:49 -0300

    .

--- nano/teste.php
+++ nano/teste.php
@@ -187,7 +187,7 @@
                 <img src="img/footer/twitter.png" class="img_fot_icons">
                 <img src="img/footer/instagram.png" class="img_fot_icons">
             </div>
-            <label>Sobre nós - Termos de privacidade</label>
+            <label>Sobre nós • Termos de privacidade</label>
             <label>Todos os direitos reservados ao Projeto DoIn</label>
         </footer>
 	</body>
