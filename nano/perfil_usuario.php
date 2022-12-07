944734225b04c8f51c36c7c927a349ca932d2737
nano <lanatriforce1234@gmail.com>
2022-12-06 22:48:26 -0300

    .

--- nano/perfil_usuario.php
+++ nano/perfil_usuario.php
@@ -30,15 +30,97 @@
 
                 <!-- PERFIL do usuário -->
                 <div class="div_cont_all" id="perfil">
-                    <div class="div_unir_info_user">
-                        <div class="ur_mom"></div>
-                        <div class="yellow"></div>
+                    <div class="div_unir_infoUser">
+                        <div class="div_infoUser">
+
+                            <div class="div_cadaInfo">
+                                <label id="nome">Nome: </label>
+                                <p>Fulano de Ciclano Mariano do Valmirando Valuriando</p>
+                            </div>
+
+                            <div class="div_cadaInfo">
+                                <label id="genero">Gênero: </label>
+                                <p>Outros</p>
+                            </div>
+
+                            <div class="div_cadaInfo">
+                                <label id="contato">Para contato: </label>
+                                <p>(não especificado)</p>
+                            </div>
+
+                            <div class="div_cadaInfo">
+                                <label id="ranking">Ranking:</label>
+                                <p>0º</p>
+                            </div>
+                        </div>
+
+                        <div class="div_imgUser">
+                            <img id="user_img" src="img/lana.jpg">
+                        </div>
+                    </div>
+
+                    <div class="div_unir_infoUser div_unir_baixo">
+                        <div class="div_infoUser div_bio">
+
+                            <label>Biografia</label>
+                            <p>Só o universo sabe o dia do amanhã, n aguento mais esse tcc pqp</p>
+
+                        </div>
+
+                        <div class="div_imgUser div_infoPlus">
+
+                            <label>Informações gerais</label>
+
+                            <div class="div_cadaInfo">
+                                <label class="label_quant">Quantidade doada: </label>
+                                <p class="p_quant">Sua mãe</p>
+                            </div>
+
+                            <div class="div_cadaInfo sem_mg_top">
+                                <label class="label_quant">Quantidade trocada: </label>
+                                <p class="p_quant">Seu irmão</p>
+                            </div>
+
+                            <div class="div_cadaInfo">
+                                <label class="label_quant">Data de criação: </label>
+                                <p class="p_quant">Sua tia nasceu hj</p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
+                <!-- PRODUTOS do usuário -->
                 <div class="div_cont_all" id="product">
-                    <div class="yellow">
-                        <p class="name">a</p>
+                    <div class="div_tituloUser">
+                        <label class="tituloUser">Fulano de Ciclano Mariano do Valmirando</label>
+
+                        <div class="div_produto">
+                            <button class="tituloUser label_produto">Todos</button>
+                            <button class="tituloUser label_produto">Doação</button>
+                            <button class="tituloUser label_produto">Troca</button>
+                        </div>
+
+                        <div class="all_produto">
+                            <div class="div_imgUser">
+                                <img id="user_img" src="img/lana.jpg">
+                            </div>
+                            <div class="div_imgUser">
+                                <img id="user_img" src="img/lana.jpg">
+                            </div>
+                            <div class="div_imgUser">
+                                <img id="user_img" src="img/lana.jpg">
+                            </div>
+                            <div class="div_imgUser">
+                                <img id="user_img" src="img/lana.jpg">
+                            </div>
+                            <div class="div_imgUser">
+                                <img id="user_img" src="img/lana.jpg">
+                            </div>
+                            <div class="div_imgUser">
+                                <img id="user_img" src="img/lana.jpg">
+                            </div>
+                        </div>
                     </div>
                 </div>
 
